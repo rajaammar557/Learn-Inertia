6bcb60b3dc6cb5f9a82825b78dc83929eed106f1
Ammar <rajaammar557@gmail.com>
2024-09-30 20:49:11 +0500

    Preserve the Scroll Position

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,10 +1,26 @@
 <script setup>
-import Nav from '../Shared/Nav.vue' 
+import Nav from '../Shared/Nav.vue'
+import { Link } from '@inertiajs/vue3'
+
+defineProps({
+    time: String
+})
 </script>
 
 <template>
     <div>
         <Nav />
         <h1>Home</h1>
+        <div class="time">
+            <h2>The current time is {{ time }}</h2>
+            <Link href="/" preserve-scroll>Refresh</Link>
+        </div>
     </div>
 </template>
+
+<style scoped>
+.time {
+    margin: 20rem 0 0 0;
+}
+</style>
+    
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Home');
+    return Inertia::render('Home', [
+        'time' => now()->toTimeString()
+    ]);
 });
 
 Route::get('/about', function () {
