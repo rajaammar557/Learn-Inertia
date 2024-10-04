ada88ceb425f56dfc7cbd3f4f5a9051e82c1b340
Ammar <rajaammar557@gmail.com>
2024-10-04 20:46:43 +0500

    Global Component Registration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import './bootstrap';
 import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/vue3'
+import { createInertiaApp, Link } from '@inertiajs/vue3'
 import NProgress from 'nprogress'
 import { router } from '@inertiajs/vue3'
 
@@ -12,6 +12,7 @@ createInertiaApp({
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
+      .component('Link', Link)
       .mount(el)
   },
 })
