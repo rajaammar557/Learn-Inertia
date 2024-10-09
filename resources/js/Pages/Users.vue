f6ae318a498c4067d1feb87dc944090250d95213
Ammar <rajaammar557@gmail.com>
2024-10-09 21:29:16 +0500

    Create Pagination

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,15 +1,27 @@
 <script setup>
+import Pagination from "../Shared/Pagination.vue";
 defineProps({
-    users: Array
+    users: Object
 })
 </script>
 <template>
 
     <Head title="Users" />
     <h1 class="text-4xl">Users</h1>
-    <div>
-        <ul>
-            <li v-for="user in users" key="user.id" v-text="user.name"></li>
-        </ul>
+
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg my-8">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500">
+            <tbody>
+                <tr class="bg-white border-b hover:bg-gray-50" v-for="user in users.data" key="user.id">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                        {{ user.name }}
+                    </th>
+                    <td class="px-6 py-4">
+                        <Link href="#" class="font-medium text-blue-600 hover:underline">Edit</Link>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
+    <pagination class="my-5" :links="users.links" />
 </template>
---
+++ resources/js/Shared/Pagination.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    links: Array
+})
+</script>
+<template>
+    <div>
+        <Component :is="link.url && !link.active ? 'Link' : 'span'" v-for="link in links" :href="link.url"
+            v-html="link.label" class="px-4 py-2 border bg-white shadow-2xl rounded"
+            :class="[link.active ? 'text-blue-600' : '', !link.url ? 'text-gray-500' : '']" />
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
